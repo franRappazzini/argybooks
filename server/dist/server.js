f332c67b50dacfc96c9118c4faa0b86ae82c6e2d
Francisco Rappazzini <fran_rappa@outlook.com>
2022-09-22 14:03:50 -0300

    fix - config to deploy

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,10 +1,32 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv = __importStar(require("dotenv"));
 const author_1 = __importDefault(require("./routes/author"));
 const aws_1 = __importDefault(require("./routes/aws"));
 const book_1 = __importDefault(require("./routes/book"));
@@ -17,6 +39,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const review_1 = __importDefault(require("./routes/review"));
 const db_1 = require("./db/db");
 const user_1 = __importDefault(require("./routes/user"));
+dotenv.config();
 const server = (0, express_1.default)();
 server.use((0, cors_1.default)());
 server.use(express_1.default.json());
@@ -32,8 +55,8 @@ server.use("/user", user_1.default);
 server.use("/author", author_1.default);
 server.use("/review", review_1.default);
 server.use("/favorite", favorite_1.default);
-const port = 3001;
+const port = process.env.PORT || 3001;
 server.listen(port, () => {
-  db_1.sequelize.sync({ force: false });
-  console.log("Server listening on port:", port);
+    db_1.sequelize.sync({ force: false });
+    console.log("Server listening on port:", port);
 });
